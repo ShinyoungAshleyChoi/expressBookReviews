18844c18c4523732681396730da61abe01279afe
shinyoungashleychoi <choishin0@gmail.com>
2023-04-02 02:25:59 -0400

    mid commit

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
@@ -30,8 +29,12 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    for ( const book in books) {
+        if ( books[book]['title'] === req.params.title ) {
+            return res.status(200).json(books[book])
+        }
+    }
+  return res.status(204).json({"message": "There's no book"});
 });
 
 //  Get book review
