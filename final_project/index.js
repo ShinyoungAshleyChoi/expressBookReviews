d6267d8e92ff82240daa6786e05ae5f305b56350
shinyoungashleychoi <choishin0@gmail.com>
2023-04-02 01:50:15 -0400

    add verify token

--- final_project/index.js
+++ final_project/index.js
@@ -10,11 +10,28 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+const JWT_SECRET = 'itsverysecret'
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+  try {
+    req.decoded = jwt.verify(req.headers.authorization, JWT_SECRET)
+    return next();
+  }
+  
+  catch(error) {
+    if (error.name === 'TokenExpireError') {
+      return res.status(419).json({
+        code: 419,
+        message: 'The token is expired.'
+      });
+    }
+   return res.status(401).json({
+     code: 401,
+     message: 'The token is invalid.'
+   });
+  }
 });
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
